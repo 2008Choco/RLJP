9a29fbbe291491478b13495df08303b69819f96a
2008Choco <hawkeboyz2@hotmail.com>
2026-05-02 20:06:19 -0400

    Define a module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module wtf.choco.rljp {
+    requires java.base;
+    requires static org.jetbrains.annotations;
+
+    exports wtf.choco.rljp;
+    exports wtf.choco.rljp.structures;
+    exports wtf.choco.rljp.structures.properties;
+    exports wtf.choco.rljp.structures.uniqueid;
+}
